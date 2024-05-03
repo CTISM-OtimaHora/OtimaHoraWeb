3880ecf54d5501918990fbb6a628e17f7d7c0491
ArturCSegat <arturcamerasegat@gmail.com>
2024-05-03 16:10:54 -0300

    arrumei umas coisas

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,19 +1,19 @@
 function toggleStatus(cell) {
-   if (cell.classList.contains("yes")) {
-      cell.classList.remove("yes");
-      cell.classList.add("maybe");
-      cell.textContent = "Maybe";
-   } else if (cell.classList.contains("maybe")) {
-      cell.classList.remove("maybe");
-      cell.classList.add("no");
-      cell.textContent = "NO";
-   } else if (cell.classList.contains("no")) {
-      cell.classList.remove("no");
-      cell.classList.add("yes");
-      cell.textContent = "YES";
+   if (cell.classList.contains("sim")) {
+      cell.classList.remove("sim");
+      cell.classList.add("talvez");
+      cell.textContent = "TALVEZ";
+   } else if (cell.classList.contains("talvez")) {
+      cell.classList.remove("talvez");
+      cell.classList.add("nao");
+      cell.textContent = "NAO";
+   } else if (cell.classList.contains("nao")) {
+      cell.classList.remove("nao");
+      cell.classList.add("sim");
+      cell.textContent = "SIM";
    } else {
-      cell.classList.add("yes");
-      cell.textContent = "YES";
+          cell.classList.add("sim");
+          cell.textContent = "SIM";
    }
 }
 
@@ -21,4 +21,34 @@ function toggleStatusCol(col_idx) {
     for (let i = 0; i < 5; i ++) {
         toggleStatus(document.getElementById(`${i}-${col_idx}`));
     }
+}
+
+function add_professor() {
+    const prof_name = document.getElementById("professor-input").value
+    document.getElementById("professor-input").value = ""
+
+    const params = new URLSearchParams(window.location.search)
+
+    fetch(`http://localhost:3000/add-professor/${params.get("curso_id")}/${params.get("turma_id")}`, 
+        {
+            body: JSON.stringify(prof_name),
+            method: "POST",
+            credentials: "include"
+        }
+    ).then()
+}
+
+function add_disciplina() {
+    const dis_name = document.getElementById("disciplina-input").value
+    document.getElementById("disciplina-input").value = ""
+
+    const params = new URLSearchParams(window.location.search)
+
+    fetch(`http://localhost:3000/add-disciplina/${params.get("curso_id")}/${params.get("turma_id")}`, 
+        {
+            body: JSON.stringify(dis_name),
+            method: "POST",
+            credentials: "include"
+        }
+    ).then()
 }
